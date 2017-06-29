f79b99c7685d309a2dd4c22f070b6ad9bd46d375
Chen Chen <mail@roychan.org>
2017-06-29 21:11:27 +0800

    Update subscriptions

--- fetch.py
+++ fetch.py
@@ -10,7 +10,7 @@ def fetch():
     CONF_FILE = os.path.join(os.path.dirname(__file__), "config.yaml")
     FEED_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
     GENERATE_FILE = os.path.join(os.path.dirname(__file__), "gen.yaml")
-    DATE_RANK = 14
+    DATE_RANK = 30
 
     fetch_time = datetime.datetime.now()
 
